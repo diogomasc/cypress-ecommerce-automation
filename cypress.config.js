35785af621b375789d7226d3b819c1ca3e9c9b4d
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2026-03-01 00:47:18 -0300

    fix: update baseUrl format and add blockHosts for analytics requests

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,10 @@ export default defineConfig({
       // implement node event listeners here
     },
     // Define a URL base para os testes, para que possamos usar caminhos relativos nos testes
-    baseUrl: "https://sauce-demo.myshopify.com/",
+    baseUrl: "https://sauce-demo.myshopify.com",
     // Não vai limpar o estado entre os testes por padrão, ou seja, o usuário vai permanecer logado entre os testes
     testIsolation: false,
+    // Bloqueia as requisições para os domínios de analytics e monitoramento, para evitar que eles causem falhas nos testes
+    blockHosts: ["*google-analytics.com", "*://shopify.com"],
   },
 });
