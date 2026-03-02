9ae1389a7ac94b32ad9209e11e0b2a3f07d15a48
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2026-03-02 18:07:57 -0300

    refactor: simplify comments in Cypress configuration and remove retry settings

--- cypress.config.js
+++ cypress.config.js
@@ -12,12 +12,7 @@ export default defineConfig({
     baseUrl: "https://sauce-demo.myshopify.com",
     // Não vai limpar o estado entre os testes por padrão, ou seja, o usuário vai permanecer logado entre os testes
     testIsolation: false,
-    // Bloqueia as requisições para os domínios de analytics e monitoramento, para evitar que eles causem falhas nos testes
+    // Bloqueia as requisições para os domínios de analytics e monitoramento
     blockHosts: ["*google-analytics.com", "*://shopify.com"],
-    // Configura o Cypress para tentar novamente os testes que falharem, para aumentar a estabilidade dos testes em casos de falhas intermitentes
-    retries: {
-      runMode: 2, // Número de tentativas de retry quando os testes falharem durante a execução normal (cypress run)
-      openMode: 1, // Número de tentativas de retry quando os testes falharem durante a execução interativa (cypress open)
-    },
   },
 });
