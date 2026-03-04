9fbad9de1d57ab0af25ef5b607af441c95c049ef
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2026-03-04 02:24:23 -0300

    feat: integrate Faker.js for dynamic user data generation; enhance cart management tests and add user registration scenarios

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+
 // Custom command para ignorar erros de requisições externas (como /api/collect) que são monitorados pelo Cypress e podem causar falhas nos testes
 Cypress.Commands.add("ignoreCollectError", () => {
   cy.on("uncaught:exception", (err) => {
@@ -18,4 +20,40 @@ Cypress.Commands.add("cleanCart", () => {
     url: "/cart/clear.js",
     failOnStatusCode: false,
   });
+});
+
+// Comando customizado para criar uma conta de usuário
+Cypress.Commands.add("createAccount", () => {
+  const user = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({ length: 12 }),
+  };
+
+  // 1. Navegação e preenchimento
+  cy.visit("/account/register", { failOnStatusCode: false });
+
+  cy.get("#first_name").should("be.visible").type(user.firstName);
+  cy.get("#last_name").type(user.lastName);
+  cy.get("#email").type(user.email);
+  cy.get("#password").type(user.password);
+
+  // 2. Submissão
+  cy.get(".action_bottom").contains("Create").click();
+
+  // 3. Validação de Fluxo
+  cy.url().should("include", "/");
+
+  // 4. Validação de Persistência na conta
+  cy.visit("/account");
+  cy.get(".sidebar .customer-name", { timeout: 10000 })
+    .should("be.visible")
+    .and("contain", `${user.firstName} ${user.lastName}`);
+
+  // 5. Faz logout para garantir que o estado da conta não interfira em outros testes
+  cy.contains("a", "Log Out").click();
+
+  // 6. Retorna o objeto para uso nos testes
+  return cy.wrap(user);
 });
